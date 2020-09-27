a40caaa5a60d41e8056d8316979ec3fe4bc2cc36
Waleed Ali Khan <waleedomore@gmail.com>
2020-09-27 08:24:30 +0500

    VueJS Drag and Drop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="flexbox">
+      <Board id="board-1">
+        <Card id="card-1" draggable="true">
+          <p>Card one</p>
+        </Card>
+      </Board>
+
+      <Board id="board-2">
+        <Card id="card-2" draggable="true">
+          <p>Card two</p>
+        </Card>
+      </Board>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from '@/components/Board';
+import Card from '@/components/Card';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Board,
+    Card,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #f3f3f3;
+}
+
+.flexbox {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 760px;
+  height: 100vh;
+  overflow: hidden;
+  margin: 0 auto;
+  padding: 15px;
+
+  .board {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 300px;
+    background: #313131;
+    padding: 15px;
+
+    .card {
+      padding: 15px 25px;
+      background: #f3f3f3;
+      cursor: pointer;
+      margin-bottom: 15px;
+    }
+  }
 }
 </style>
---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :id="id" class="board" @dragover.prevent @drop.prevent="drop">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: ['id'],
+  methods: {
+    drop: (e) => {
+      const card_id = e.dataTransfer.getData('card_id');
+
+      const card = document.getElementById(card_id);
+      card.style.display = 'block';
+
+      e.target.appendChild(card);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
